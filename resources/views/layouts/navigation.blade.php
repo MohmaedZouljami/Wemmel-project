04f964d60d49c019be54d584e605ac1ec58661bb
Mohamed Zouljami <mohamed.zljm@gmail.com>
2026-05-20 10:06:34 +0200

    Navigatie bijgewerkt met Home-link

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -14,7 +14,12 @@
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+
                     </x-nav-link>
+                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                        Home
+                    </x-nav-link>
+
                 </div>
             </div>
 
