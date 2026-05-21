1c5dcffa8bf20b7a86345e44aa9c335638ff6763
Mohamed Zouljami <mohamed.zljm@gmail.com>
2026-05-21 11:03:47 +0200

    contact bericht model aangemaakt

--- app/Models/ContactBericht.php
+++ app/Models/ContactBericht.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ContactBericht extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'contact_berichten';
+
+    protected $fillable = [
+        'naam',
+        'email',
+        'onderwerp',
+        'bericht',
+    ];
 }
